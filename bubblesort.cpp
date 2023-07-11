abb8a538ffe0dcd10449801c89702f145ac8cff8
sarveshvasan03 <136417300+sarveshvasan03@users.noreply.github.com>
2023-07-11 10:21:46 +0530

    Update bubblesort.cpp

--- bubblesort.cpp
+++ bubblesort.cpp
@@ -1,37 +1,46 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
-  
-void bubbleSort(int arr[], int n)
-{
-    int i, j;
-    bool swapped;
-    for (i = 0; i < n - 1; i++) {
-        swapped = false;
-        for (j = 0; j < n - i - 1; j++) {
-            if (arr[j] > arr[j + 1]) {
-                swap(arr[j], arr[j + 1]);
-                swapped = true;
+
+class BubbleSort {
+private:
+    int* arr;
+    int size;
+
+public:
+    BubbleSort(int* array, int length) {
+        arr = array;
+        size = length;
+    }
+
+    void sort() {
+        for (int i = 0; i < size - 1; ++i) {
+            for (int j = 0; j < size - i - 1; ++j) {
+                if (arr[j] > arr[j + 1]) {
+                    // Swap elements if they are in the wrong order
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
             }
         }
-
-        if (swapped == false)
-            break;
     }
-}
-  
-void printArray(int arr[], int size)
-{
-    int i;
-    for (i = 0; i < size; i++)
-        cout << " " << arr[i];
-}
 
-int main()
-{
+    void display() {
+        cout << "Sorted array: ";
+        for (int i = 0; i < size; ++i) {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
+    }
+};
+
+int main() {
     int arr[] = { 64, 34, 25, 12, 22, 11, 90 };
-    int N = sizeof(arr) / sizeof(arr[0]);
-    bubbleSort(arr, N);
-    cout << "Sorted array: \n";
-    printArray(arr, N);
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    BubbleSort bubble(arr, size);
+    bubble.sort();
+    bubble.display();
+
     return 0;
 }
