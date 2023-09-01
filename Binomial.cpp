2b5e56655aaf2904f54eca937b8ced61f21c3e6e
sarveshvasan03 <136417300+sarveshvasan03@users.noreply.github.com>
2023-09-01 10:08:07 +0530

    Create Binomial.cpp

---
+++ Binomial.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int binomialCoefficient(int n, int k) {
+    int dp[n + 1][k + 1];
+
+    for (int i = 0; i <= n; ++i) {
+        for (int j = 0; j <= min(i, k); ++j) {
+            if (j == 0 || j == i)
+                dp[i][j] = 1;
+            else
+                dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+        }
+    }
+
+    return dp[n][k];
+}
+
+int main() {
+    int n, k;
+    cout << "Enter the values of n and k: ";
+    cin >> n >> k;
+
+    int result = binomialCoefficient(n, k);
+    cout << "The binomial coefficient C(" << n << ", " << k << ") = " << result << endl;
+
+    return 0;
+}
